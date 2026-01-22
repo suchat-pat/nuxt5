a7d550b24b4a7fb43bfa2b04f4e492500118609b
suchat55210 <suchacce@gmail.com>
2026-01-22 16:18:22 +0700

    test

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    5555
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    iiii
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
