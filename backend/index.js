629f01684cb21478c918b5636c9b03d7de8ae6f6
peet5228 <ppeet5228@gmail.com>
2026-01-22 16:33:25 +0700

    first

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const fileupload = require('express-fileupload')
+const cors = require('cors')
+const app = express()
+const path = require('path')
+
+app.use(cors ({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(fileupload())
+app.use('/uploads',express.static(path.join('uploads')))
+
+app.use((req,res) => res.status(404).json({message:'กำลังปรับปรุงระบบ!'}))
+app.listen(3001 , () => console.log('Server Running On Port 3001'))
