2e69c9054f799e7d1832f4e1549a45d5e4ef17a7
suchat55210 <suchacce@gmail.com>
2026-01-23 09:51:08 +0700

    testPat

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,8 +1,60 @@
 <template>
-    5555
+    <v-app>
+        <v-app-bar color="#7d0c14" class="py-2">
+            <v-app-bar-nav-icon @click="drawer = !drawer" variant="text"></v-app-bar-nav-icon>
+            <v-toolbar-title>ระบบประเมินบุคลากรวิทยาลัยเทคนิคน่าน</v-toolbar-title>
+            <p>ผู้ใช้งาน : {{ user.first_name }} {{ user.last_name }} <br /> {{ user.role }}</p>&nbsp;&nbsp;
+            <v-btn @click="logout" class="bg-white">ออกจากระบบ</v-btn>&nbsp;
+        </v-app-bar>
+        <ClientOnly>
+        <v-navigation-drawer v-model="drawer" color="#404040" app :temporary="isMobile" :permanent="!isMobile">
+            <v-list>
+                <v-list-item v-for="item in navitem" :key="item.title" :to="item.to">
+                    <v-list-item-title>
+                        {{ item.title }}
+                    </v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        </ClientOnly>
+        <v-main>
+            <v-container fluid>
+                <NuxtPage />
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script setup lang="ts">
+import { useDisplay } from 'vuetify';
+const {mdAndDown} = useDisplay()
+const isMobile = computed(() => mdAndDown.value)
+const drawer = ref(false)
+const user = ref<any>({})
+
+const logout = async () =>{
+    if(!confirm('ต้องการออกจากระบบใช่หรือไม่'))return
+    localStorage.removeItem('token')
+    navigateTo('/')
+}
+
+const roles = [
+    //staff
+    {title:'หน้าหลัก',to:'/Staff/',role:'ผ่ายบุคลากร'},
+
+    //commit
+    {title:'รายชื่อผู้รับการประเมิน',to:'/Committee/',role:'กรรมการประเมิน'},
+
+    //eva
+    {title:'หน้าหลัก',to:'/Evaluatee/',role:'ผู้รับการประเมินผล'},
+]
+const navitem = computed(() => 
+    roles.filter((item) => item.role.includes(user.value.role))
+)
+
+const fetchUser = async () =>{
+    
+}
 
 </script>
 
